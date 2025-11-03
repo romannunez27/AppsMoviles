c53f4a62f2bb4d36eb35d32dd4730370f2880802
Román <roma.nunez@duocuc.cl>
2025-11-03 18:06:52 -0300

    push

--- app/src/main/java/com/example/pasteleriamilsaboresapp/data/model/Producto.kt
+++ app/src/main/java/com/example/pasteleriamilsaboresapp/data/model/Producto.kt
@@ -1,11 +1,14 @@
 package com.example.pasteleriamilsaboresapp.data.model
 
+import com.google.firebase.firestore.PropertyName
+
 
 data class Producto(
     val id: String = "",
     val nombre: String = "",
     val precio: Double = 0.0,
-    val imagen:String = "",
+    @get:PropertyName("image") @set:PropertyName("image")
+    var imagen:String = "",
     val stock: Int = 0,
     val mensajeDedicatoria: Boolean = false,
     val agregarVela:Boolean = false
